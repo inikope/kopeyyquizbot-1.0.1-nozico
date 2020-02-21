e4a0c7acae07ab6310b44edbb40237879ca2c5a5
inikope <59614495+inikope@users.noreply.github.com>
2020-02-21 11:39:10 +0700

    Try link3

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ app.get('/', (req, res) => {
     function startSoal(replyToken,nomor){
         const linkSatu = "https://1.bp.blogspot.com/-Qot9XEIeoxs/Xk69IfL1VnI/AAAAAAAAJBM/yGAskJGeey8farTlSwJoOvfaeL4GToDQgCLcBGAsYHQ/s1600/KQBot-1.jpg";
         const linkDua = "https://audio.jukehost.co.uk/3147c1b3d541ce112d79a11c26f9e77b3c6f4d75/ea92850ed5e";
-        const linkTiga = "https://audio.jukehost.co.uk/3147c1b3d541ce112d79a11c26f9e77b3c6f4d75/3678da37770";
+        const linkTiga = "https://drive.google.com/uc?export=open&id=1oZgFnnuypyGnAPT3iEKfNKbaZn5tYy8x";
         const linkEmpat = "https://1.bp.blogspot.com/-qreeSzd8eD4/Xk7Gw1T8PTI/AAAAAAAAJBY/_PgDKZfQofwhWiw0SsJ7G7FD41Jpp5txwCLcBGAsYHQ/s1600/KQBot-4.jpg";
         const linkLima = "https://audio.jukehost.co.uk/3147c1b3d541ce112d79a11c26f9e77b3c6f4d75/2557d472608";
         const linkEnam = "https://audio.jukehost.co.uk/3147c1b3d541ce112d79a11c26f9e77b3c6f4d75/f9b55b5fbb4";
