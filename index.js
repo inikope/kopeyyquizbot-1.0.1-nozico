5ecc386fb77939ec57f573e39ecda22017749d1e
inikope <59614495+inikope@users.noreply.github.com>
2020-02-22 21:37:38 +0700

    Update index.js

--- index.js
+++ index.js
@@ -214,8 +214,6 @@ app.get('/', (req, res) => {
         } else if(receivedMessage.includes(" ")){
             var index = receivedMessage.indexOf(' ');
             var [first, second] = [receivedMessage.slice(0, index), receivedMessage.slice(index + 1)];
-            console.log("first: "+first );
-            console.log("second: "+second.toLowerCase() );
             switch (first){
                 case '/nyolot':
                     return replyText(event.replyToken, NyolotBalik + second);
